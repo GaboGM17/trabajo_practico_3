a319c9b5e5779edc11837f8b87eff00686a957c3
Carlosaguirre5 <carlos199787@hotmail.es>
2024-03-11 09:54:57 -0600

    tercera funcion

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+
 def saludo_usuario():
     nombre = input("Ingrese su nombre")
     return f"Hola {nombre}!, es un placer saludarte"
@@ -13,3 +14,28 @@ numero = int(input("Ingrese un número: "))
 for i in range(1, numero + 1):
   # Mostrar cada número en la consola
   print(i)
+
+
+
+def calcular(operacion):
+    numero1 = int(input("Ingrese el primer numero: "))
+    numero2 = int(input("Ingrese el segundo numero: "))
+
+    if operacion == '+':
+        resultado = numero1 + numero2
+    elif operacion == '-':
+        resultado = numero1 - numero2
+    elif operacion == '*':
+        resultado = numero1 * numero2
+    elif operacion == '/':
+        resultado = numero1 / numero2
+    else:
+        resultado = "Operación no válida"
+
+    return resultado
+
+operacion_elegida = input("Ingrese la operación que desea realizar (+, -, *, /): ")
+
+resultado = calcular(operacion_elegida)
+print("El resultado de la operación es:", resultado)
+
